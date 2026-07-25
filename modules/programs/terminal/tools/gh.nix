c7919e0ff0d2bf13ce1512171342ff839e3bf535
eveeifyeve <kbkk7250@outlook.com>
2026-07-26 04:04:00 +1000

    chore(hm): add gh-enhance to gh-dash

--- modules/programs/terminal/tools/gh.nix
+++ modules/programs/terminal/tools/gh.nix
@@ -1,17 +1,29 @@
 {
-  homeManager.modules.base = {
-    programs.gh-dash.enable = true;
-    programs.gh = {
-      enable = true;
-      settings = {
-        git_protocol = "ssh";
-        prompt = "enabled";
+  homeManager.modules.base =
+    { pkgs, ... }:
+    {
+      programs.gh-dash = {
+        enable = true;
+        settings.keybinds.prs = [
+          {
+            key = "T";
+            command = "tmux new-window 'gh enhance -R {{.RepoName}} {{.PrNumber}}'";
+          }
+        ];
+      };
+      programs.gh = {
+        enable = true;
+        settings = {
+          git_protocol = "ssh";
+          prompt = "enabled";
 
-        aliases = {
-          co = "pr checkout";
-          pv = "pr view";
+          aliases = {
+            co = "pr checkout";
+            pv = "pr view";
+          };
         };
       };
+
+      home.packages = [ pkgs.gh-enhance ];
     };
-  };
 }
