3cd4ed6fe02d9f52f848a4b0db5b22a451959e8f
eveeifyeve <kbkk7250@outlook.com>
2026-07-26 07:48:38 +1000

    chore(darwin): cleanup linux-builder

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -1,29 +1,13 @@
-{ lib, ... }:
-
-let
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-  ];
-in
 {
   darwin.modules.base = {
-    nix.settings.extra-platforms = systems;
     nix.linux-builder = {
       enable = false;
-      inherit systems;
       ephemeral = true;
-      mandatoryFeatures = [ "nixos-test" ];
-      config = {
-        boot.binfmt.emulatedSystems = builtins.filter (
-          system: system != "aarch64-linux" && lib.strings.hasSuffix "-linux" system
-        ) systems; # Allows to emulated for building for more archtechures
-        nix.settings.experimental-features = "nix-command flakes";
-        security.sudo.wheelNeedsPassword = false; # Allows the ability to ssh into the builder for any reason.
-        users.users."builder".extraGroups = [ "wheel" ];
-        virtualisation.diskSize = lib.mkForce (1024 * 100);
-        virtualisation.cores = 5;
-      };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
     };
   };
 }
