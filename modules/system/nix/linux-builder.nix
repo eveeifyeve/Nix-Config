632de8cb907e7f8e93ed90c1965c9422f1ccca43
eveeifyeve <kbkk7250@outlook.com>
2026-08-01 12:37:55 +1000

    fix(linux-builder): platforms and enable

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -1,13 +1,16 @@
 {
-  darwin.modules.base = {
-    nix.linux-builder = {
-      enable = false;
-      ephemeral = true;
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+  darwin.modules.base =
+    { pkgs, ... }:
+    {
+      nix.linux-builder = {
+        enable = true;
+        package = pkgs.darwin.linux-builder;
+        ephemeral = true;
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+      };
     };
-  };
 }
