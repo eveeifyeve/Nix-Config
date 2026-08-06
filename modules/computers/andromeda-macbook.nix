5cacff886ac55a832b3db16174f819133c04db76
eveeifyeve <kbkk7250@outlook.com>
2026-08-07 00:47:11 +1000

    fix(macbook-adromeda): add root and admin as trusted user groups
    TODO: make this work with `users.<user>.trusted` instead.

--- modules/computers/andromeda-macbook.nix
+++ modules/computers/andromeda-macbook.nix
@@ -2,6 +2,10 @@
 {
   darwin.configurations.andromeda-macbook.module = {
     nixpkgs.hostPlatform.system = "aarch64-darwin";
+    nix.settings.trusted-users = [
+      "root"
+      "@admin"
+    ];
     system.stateVersion = 7;
     imports = with config.darwin.modules; [
       base
