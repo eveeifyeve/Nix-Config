5fc05dda8afca8a7c8cfac0ddf76815eaca80684
eveeifyeve <kbkk7250@outlook.com>
2026-08-29 00:59:27 +1000

    fix(darwin): darwin linux builder

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -1,16 +1,13 @@
 {
-  darwin.modules.base =
-    { pkgs, ... }:
-    {
-      nix.linux-builder = {
-        enable = true;
-        package = pkgs.darwin.linux-builder;
-        ephemeral = true;
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-        config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
-      };
+  darwin.modules.base = {
+    nix.linux-builder = {
+      enable = true;
+      ephemeral = true;
+      # systems = [
+      #   "x86_64-linux"
+      #   "aarch64-linux"
+      # ];
+      # config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
     };
+  };
 }
