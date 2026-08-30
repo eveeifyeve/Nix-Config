c9e7525e4746a12d0e42dd597ae477ddc7402bb8
eveeifyeve <kbkk7250@outlook.com>
2026-08-31 00:50:03 +1000

    chore(netdromeda): switch back to nixos due to some issues with finix

--- modules/computers/netdromeda.nix
+++ modules/computers/netdromeda.nix
@@ -1,18 +1,12 @@
-{ inputs, config, ... }:
+{ config, ... }:
 {
-  finix.configurations.netdromeda.module = {
-    #nixpkgs.hostPlatform = "x86_64-linux";
-
-    nixpkgs.pkgs = import inputs.nixpkgs (
-      {
-        system = "x86_64-linux";
-      }
-      // config.nixpkgs.args
-    );
+  nixos.configurations.netdromeda.module = {
+    nixpkgs.hostPlatform = "x86_64-linux";
 
     imports = [
       config.nixos.modules.base
-      config.finix.modules.base
+      config.nixos.modules.nixos
+      #config.finix.modules.base
     ];
   };
 }
