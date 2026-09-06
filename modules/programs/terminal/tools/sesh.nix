8eb73acf8a20a1aa669b120a75abd0d181a855c5
eveeifyeve <kbkk7250@outlook.com>
2026-09-06 12:35:09 +1000

    chore(hm): update sesh session layouts and tmux mappings

--- modules/programs/terminal/tools/sesh.nix
+++ modules/programs/terminal/tools/sesh.nix
@@ -7,7 +7,12 @@
           {
             name = "Config";
             path = "/etc/nixos";
-            startup_command = "bash -c 'tmux split-window -v -l 25%; tmux select-pane -t .0; exec nvim .'";
+            startup_command = "tmux split-window -h && tmux send-keys -t 1 'opencode' C-m && tmux select-pane -t 0 && tmux split-window -v -p 25 && tmux select-pane -t 0";
+          }
+          {
+            name = "Projects";
+            path = "~/projects/*";
+            startup_command = "tmux split-window -h && tmux send-keys -t 1 'opencode' C-m && tmux select-pane -t 0 && tmux split-window -v -p 25 && tmux select-pane -t 0";
           }
         ];
       };
