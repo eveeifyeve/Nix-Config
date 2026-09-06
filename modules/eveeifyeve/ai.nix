3d94d36b5666d46e8f87ee5fdee4b30282f7bcc0
eveeifyeve <kbkk7250@outlook.com>
2026-09-06 10:19:06 +1000

    chore(eveeifyeve): remove avante
    Removed in b671c83f

--- modules/eveeifyeve/ai.nix
+++ modules/eveeifyeve/ai.nix
@@ -6,15 +6,5 @@
       model = "alibaba/qwen3-coder-30b-a3b-instruct";
       provider.openrouter.options.apiKey = "{file:${hmArgs.config.sops.secrets.openrouter-apikey-eveeifyeve.path}}";
     };
-
-    programs.nixvim.plugins.avante.settings = {
-      provider = "openrouter";
-      providers.openrouter = {
-        __inherited_from = "openai";
-        endpoint = "https://openrouter.ai/api/v1";
-        model = "qwen3-coder-30b-a3b-instruct";
-        api_key_name = "cmd: cat ${hmArgs.config.sops.secrets.openrouter-apikey-eveeifyeve.path}";
-      };
-    };
   };
 }
