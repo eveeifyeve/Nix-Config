9752a7177398834e80e34b9a21d0e52a2fe687b8
eveeifyeve <kbkk7250@outlook.com>
2026-09-06 10:20:59 +1000

    feat(eveeifyeve): switch to glm and deepseek (small) flash models

--- modules/eveeifyeve/ai.nix
+++ modules/eveeifyeve/ai.nix
@@ -3,7 +3,8 @@
     sops.secrets.openrouter-apikey-eveeifyeve.sopsFile = ./secrets/openrouter-apikey;
 
     programs.opencode.settings = {
-      model = "alibaba/qwen3-coder-30b-a3b-instruct";
+      model = "~z-ai/glm-flash-latest";
+      small_model = "~deepseek/deepseek-v4-flash-latest";
       provider.openrouter.options.apiKey = "{file:${hmArgs.config.sops.secrets.openrouter-apikey-eveeifyeve.path}}";
     };
   };
